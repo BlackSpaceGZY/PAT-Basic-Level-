3a1fb2f48384c1b3c2255c94daf08dee7868ef8d
LeetCode <593947521@qq.com>
2019-03-09 21:15:06 +0800

    Code file

---
+++ Code/1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int count=0,n;
+	cin>>n;
+	while(n!=1){
+      if(n%2==0)
+      	n=n/2;
+      else
+      	n=(3*n+1)/2;
+        count++;        
+	}
+  cout<<count<<endl;
+	return 0;
+}
+
